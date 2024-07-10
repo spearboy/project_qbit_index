aa6f9fe89a69aa0684bc4acf1dd9bedc169a7d0f
spearboy <95023009+spearboy@users.noreply.github.com>
2024-07-11 04:23:56 +0900

    update

--- src/components/ViewPage.jsx
+++ src/components/ViewPage.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const ViewPage = ({ currentUrl }) => {
   return (
     <div className='view_area'>
+      <p className='alert'>(http,github,)페이지 로드가 안될 시 간소화 페이지에서 확인 바랍니다.</p>
       <iframe src={currentUrl} title="View Page"></iframe>
     </div>
   );
